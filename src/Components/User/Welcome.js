93717ac868cf57c7f6d65552fb6733c356326385
rommel667 <rommel667@gmail.com>
2021-06-20 23:05:20 +0800

    welcome component update

--- src/Components/User/Welcome.js
+++ src/Components/User/Welcome.js
@@ -14,8 +14,8 @@ const Welcome = () => {
             <div class="bg-image" style={{ backgroundImage: `url(${bgImage})` }}></div>
     
             <div class="bg-text">
-                <h1>Good day Engineer</h1>
-                <h2>Test your knowledge. Track your progress. ACE YOUR BOARD EXAM.</h2>
+                <h1>Ready on your Engineering Board Exam?</h1>
+                <h2 style={{textAlign: "justify"}}>In InstaLearn. We test your knowledge. We track your progress. So you are ready to ACE YOUR BOARD EXAM.</h2>
                 <p style={{ marginTop: 40 }}>There is no substitute for hard work. -Thomas Edison</p>
                 <div style={{ marginTop: 50 }}>
                 <Button onClick={() => history.push('/register')} className="newAccountButton" variant='contained' color='primary' type='Submit'>
