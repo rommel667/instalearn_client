a1ead9989783eafa6a21098bf09252d712314de6
rommel667 <rommel667@gmail.com>
2021-06-21 08:11:06 +0800

    demo button on welcome page

--- src/Components/User/Welcome.js
+++ src/Components/User/Welcome.js
@@ -12,25 +12,32 @@ const Welcome = () => {
         <div className="welcome">
 
             <div class="bg-image" style={{ backgroundImage: `url(${bgImage})` }}></div>
-    
+
             <div class="bg-text">
                 <h1>Ready on your Engineering Board Exam?</h1>
-                <h2 style={{textAlign: "justify"}}>In InstaLearn. We test your knowledge. We track your progress. So you are ready to ACE YOUR BOARD EXAM.</h2>
+                <h2 style={{ textAlign: "justify" }}>In InstaLearn. We test your knowledge. We track your progress. So you are ready to ACE YOUR BOARD EXAM.</h2>
                 <p style={{ marginTop: 40 }}>There is no substitute for hard work. -Thomas Edison</p>
                 <div style={{ marginTop: 50 }}>
-                <Button onClick={() => history.push('/register')} className="newAccountButton" variant='contained' color='primary' type='Submit'>
-                <div style={{ display: "flex", alignItems: "center", textTransform: "none"}}>Create an Account</div>
-                </Button>
-                <div style={{ display: "flex", alignItems: "center", justifyContent: "center", gap: 5 }}>
-                <p style={{ fontWeight: "lighter" }}>Already have an account?</p>
-                <div onClick={() => history.push('/login')} style={{ cursor: "pointer" }}>Login</div>
+
+                    <div style={{ display: "flex", gap: 5, justifyContent: "center" }}>
+                        <Button onClick={() => history.push('/register')} className="newAccountButton" variant='contained' color='primary' type='Submit'>
+                            <div style={{ display: "flex", alignItems: "center", textTransform: "none", fontWeight: "bold" }}>Create an Account</div>
+                        </Button>
+                        <Button onClick={() => history.push('/register')} className="newAccountButton" variant='outlined' color='inherit' type='Submit'>
+                            <div style={{ display: "flex", alignItems: "center", textTransform: "none", fontWeight: "bold" }}>Try Demo</div>
+                        </Button>
+                    </div>
+
+                    <div style={{ display: "flex", alignItems: "center", justifyContent: "center", gap: 5 }}>
+                        <p style={{ fontWeight: "lighter" }}>Already have an account?</p>
+                        <div onClick={() => history.push('/login')} style={{ cursor: "pointer" }}>Login</div>
+                    </div>
+
                 </div>
-                
-                </div>
-                
+
             </div>
-            
-            
+
+
 
         </div>
 
