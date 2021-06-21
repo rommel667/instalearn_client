0862200f16eb77c4e8ab3ece14a136799a543508
rommel667 <rommel667@gmail.com>
2021-06-21 08:36:03 +0800

    implement demo acount registration

--- src/Components/User/Welcome.js
+++ src/Components/User/Welcome.js
@@ -3,10 +3,24 @@ import './Welcome.css'
 import bgImage from '../../assets/welcome.jpg'
 import { Button } from '@material-ui/core'
 import { useHistory } from 'react-router-dom'
+import { useMutation } from '@apollo/client'
+import gql from 'graphql-tag'
+import { useDispatch } from 'react-redux'
 
 const Welcome = () => {
 
     const history = useHistory()
+    const dispatch = useDispatch()
+
+    const [tryDemo, { data, loading }] = useMutation(TRIAL_ACCOUNT, {
+        update(proxy, result) {
+            console.log("TRYDEMO", result);
+            dispatch({ type: "LOGIN", payload: { user: result.data.tryDemo } })
+        },
+        // onError(err) {
+        //   setError(err.graphQLErrors[0].message.split(': ')[1]);
+        // }
+    })
 
     return (
         <div className="welcome">
@@ -23,7 +37,7 @@ const Welcome = () => {
                         <Button onClick={() => history.push('/register')} className="newAccountButton" variant='contained' color='primary' type='Submit'>
                             <div style={{ display: "flex", alignItems: "center", textTransform: "none", fontWeight: "bold" }}>Create an Account</div>
                         </Button>
-                        <Button onClick={() => history.push('/register')} className="newAccountButton" variant='outlined' color='inherit' type='Submit'>
+                        <Button onClick={tryDemo} className="newAccountButton" variant='outlined' color='inherit' type='Submit'>
                             <div style={{ display: "flex", alignItems: "center", textTransform: "none", fontWeight: "bold" }}>Try Demo</div>
                         </Button>
                     </div>
@@ -43,5 +57,19 @@ const Welcome = () => {
 
     )
 }
+
+
+const TRIAL_ACCOUNT = gql`
+mutation {
+    tryDemo {
+      _id
+      email
+      name
+      photo
+      token
+    }
+} 
+
+`
 
 export default Welcome
